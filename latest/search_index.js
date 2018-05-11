af0fbcf6865f1927e5a451f006f3ac8fb1f7d1bd
autodocs <autodocs>
2018-05-11 11:02:28 +0000

    build based on 886f4fc

--- latest/search_index.js
+++ latest/search_index.js
@@ -81,19 +81,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#StatsBase.fit",
-    "page": "GLM Documentation",
-    "title": "StatsBase.fit",
-    "category": "function",
-    "text": "fit(Histogram, data[, weight][, edges]; closed=:right, nbins)\n\nFit a histogram to data.\n\nArguments\n\ndata: either a vector (for a 1-dimensional histogram), or a tuple of vectors of equal length (for an n-dimensional histogram).\nweight: an optional AbstractWeights (of the same length as the data vectors), denoting the weight each observation contributes to the bin. If no weight vector is supplied, each observation has weight 1.\nedges: a vector (typically an AbstractRange object), or tuple of vectors, that gives the edges of the bins along each dimension. If no edges are provided, these are determined from the data.\n\nKeyword arguments\n\nclosed=:right: if :left, the bin intervals are left-closed [a,b); if :right (the default), intervals are right-closed (a,b].\nnbins: if no edges argument is supplied, the approximate number of bins to use along each dimension (can be either a single integer, or a tuple of integers).\n\nExamples\n\n# Univariate\nh = fit(Histogram, rand(100))\nh = fit(Histogram, rand(100), 0:0.1:1.0)\nh = fit(Histogram, rand(100), nbins=10)\nh = fit(Histogram, rand(100), weights(rand(100)), 0:0.1:1.0)\nh = fit(Histogram, [20], 0:20:100)\nh = fit(Histogram, [20], 0:20:100, closed=:left)\n\n# Multivariate\nh = fit(Histogram, (rand(100),rand(100)))\nh = fit(Histogram, (rand(100),rand(100)),nbins=10)\n\n\n\nFit a statistical model.\n\n\n\n"
-},
-
-{
     "location": "index.html#API-1",
     "page": "GLM Documentation",
     "title": "API",
     "category": "section",
-    "text": "GLM.fit"
+    "text": ""
 },
 
 {
